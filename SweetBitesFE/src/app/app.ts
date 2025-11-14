75b222a7240c1518b17c174c82553548dd5c8439
NOURSHILI-debug <nourshili25@gmail.com>
2025-11-14 20:32:13 +0100

    HF for the cookies page

--- SweetBitesFE/src/app/app.ts
+++ SweetBitesFE/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CookiesComponent } from './pages/cookies/cookies.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,CookiesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
